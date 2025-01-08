8c17c77479e7f36195ed973795523969e66cb064
Data Synthetics Lab <founder@datasynthetics.com>
2025-01-07 23:42:55 -0800

    Update models.sql
    seeding data models with atomic entities for customers and orders

--- SQL/models.sql
+++ SQL/models.sql
@@ -1,1 +1,21 @@
+CREATE TABLE IF NOT EXISTS Customers (
+    id INT PRIMARY KEY,
+    firstname VARCHAR(100),
+    lastname VARCHAR(100),
+    email VARCHAR(100),
+    status VARCHAR(20)
+    created_date TIMESTAMP
+    updated_date TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS Orders (
+    orderId INT PRIMARY KEY,
+    customerId INT,
+    orderOrigin VARCHAR(100)
+    orderValue DECIMAL(10, 2),
+    created_date TIMESTAMP 
+    FOREIGN KEY (customerId) REFERENCES Customers(id)
+);
+
+-- Relationship already defined with FOREIGN KEY in Order table
 
