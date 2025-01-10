24826a653aac9bce404b6995d85aa70d13896c5f
Data Synthetics Lab <founder@datasynthetics.com>
2025-01-09 18:23:59 -0800

    Update views.sql

--- SQL/views.sql
+++ SQL/views.sql
@@ -1,1 +1,7 @@
-
+CREATE VIEW CustomerOrders AS
+SELECT c.id AS customerId, c.name AS customerName, 
+  o.orderId, 
+  o.orderValue
+  sum(o.orderValue) OVER (Partition By o.customerId Order By o.created_date ) 
+FROM Customers c
+JOIN Orders o ON c.id = o.customerId;
