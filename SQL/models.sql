c008ea827c2c050fe2d1221c6bbb922b283e5bce
Data Synthetics Lab <founder@datasynthetics.com>
2025-01-14 18:16:04 -0800

    Update models.sql

--- SQL/models.sql
+++ SQL/models.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS Customers (
     id INT PRIMARY KEY,
-    firstname VARCHAR(100),
-    lastname VARCHAR(100),
+    firstname VARCHAR(100) NOT NULL
+    lastname VARCHAR(100) NOT NULL,
     email VARCHAR(100),
-    status VARCHAR(20)
-    created_date TIMESTAMP
+    status VARCHAR(20) NOT NULL,
+    created_date TIMESTAMP,
     updated_date TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS Orders (
     orderId INT PRIMARY KEY,
     customerId INT,
-    orderOrigin VARCHAR(100)
-    orderValue DECIMAL(10, 2),
-    created_date TIMESTAMP 
+    orderOrigin VARCHAR(100) Not NULL,
+    orderValue DECIMAL(10, 2) NOT NULL,
+    created_date TIMESTAMP NOT NULL
     FOREIGN KEY (customerId) REFERENCES Customers(id)
 );
 
