9f522d3306992f10f9a10e39497b7b5aecf5054b
Data Synthetics Lab <founder@datasynthetics.com>
2025-03-26 20:40:40 -0700

    Update views.sql
    Updated customer name field to include space as well as edited code notes

--- SQL/views.sql
+++ SQL/views.sql
@@ -1,9 +1,9 @@
 CREATE VIEW CustomerOrders AS  -- View to Synchronize Customer & Order data
 SELECT c.id AS customerId, -- unique customer id
-  concat(c.firstname, c.lastname) AS customerName, -- customer combined first and last name 
+  concat(c.firstname,'', c.lastname) AS customerName, -- customer combined first and last name 
   o.orderId, -- order unique ID
   o.orderValue AS -- dollar amount exchanged for order
-  sum(o.orderValue) OVER (Partition By o.customerId Order By o.created_date Unbounded Preceding) AS Value_to_date -- Running cumulative sum of all orders for that client threw current order
+  sum(o.orderValue) OVER (Partition By o.customerId Order By o.created_date Unbounded Preceding) AS Value_to_date -- Running cumulative sum of all orders for that client through current order
   o.created_date AS order_date -- date when customer completed the order
   date_part('month', o.created_date) AS order_month -- month when customer completed order
   date_part('year', o.created_date) AS order_year -- year when customer completed order
